6ac7bb112eb5add848e3278bc3e5cda797cd210c
Amy Williams <csamywilliams@gmail.com>
2020-02-16 10:19:53 +0000

    Add theme colours

---
+++ src/theme/theme.js
@@ -0,0 +1,11 @@
+const THEME = { 
+    goblin: {
+        color1: '#EE6352',
+        color2: '#59CD90',
+        color3: '#3FA7D6',
+        color4: '#FAC05E',
+        color5: '#F79D84'
+    }
+};
+
+export default THEME;
